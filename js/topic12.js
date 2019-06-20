1fd8c0ac31d41c7edb25012d98f0a3aef0fa8979
denisegoetz <Denisegoetz@msn.com>
2019-06-20 18:35:30 -0400

    add topic 12

---
+++ js/topic12.js
@@ -0,0 +1,11 @@
+// functions for translate - move
+function trans1() {
+    const transBox = document.getElementById('transBox1');
+    transBox.classList.toggle('trans1');
+}
+
+function trans2() {
+    const transBox = document.getElementById('transBox2');
+    transBox.classList.toggle('trans2');
+}
+  
